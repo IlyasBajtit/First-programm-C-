887591c62b90ceb5b67bbfc89e57e94535d4d3d7
IlyasBajtit <ilyas.bajtit@gmail.com>
2023-12-20 14:24:56 +0100

    initial commit

--- Exercice08/Program.cs
+++ Exercice08/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Write("Entrez le prix HT de l'objet (en Euros): ");
+double PrixHt = double.Parse(Console.ReadLine());
+
+Console.Write("Entrez le taux de TVA (en %): ");
+double TauxTva = double.Parse(Console.ReadLine());
+
+double mtTva = PrixHt * TauxTva / 100;
+double Prixobj = PrixHt + TauxTva;
+
+Console.WriteLine($"Le montatnt de la T.V.A est de : {mtTva}" +  "Euros");
+Console.WriteLine($"Le prix TTC de l'objet est de : {Prixobj}" + "Euros");
+
+
