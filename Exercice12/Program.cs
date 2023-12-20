dc8fbe63400ff61a3a35b9432883f75fb0471880
IlyasBajtit <ilyas.bajtit@gmail.com>
2023-12-20 18:20:29 +0100

    feat: add ex12

--- Exercice12/Program.cs
+++ Exercice12/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("--- Dans quelle catégorie mon enfant est-il...?");
+
+Console.Write("Entrez l'âge de votre enfant : ");
+int age = int.Parse(Console.ReadLine());
+
+Console.Write("Votre enfant est dans la catégorie : ");
+
+if (age < 3)
+{
+    Console.WriteLine("Votre enfant est trop jeune");
+}
+else if (age >= 3 && age <= 6)
+{
+    Console.WriteLine("Baby");
+}
+else if (age >= 7 && age <= 8)
+{
+    Console.WriteLine(" Poussin");
+}
+else if (age >= 9 && age <= 10)
+{
+    Console.WriteLine(" Pupille");
+}
+else if (age >= 11 && age <= 12)
+{
+    Console.WriteLine(" Minime");
+}
+else if (age == 13)
+{
+    Console.WriteLine(" Cadet");
+}
+else if (age >= 18)
+{
+    Console.WriteLine("Votre enfant n'est plus un enfant");
+}
+else
+{
+    Console.WriteLine("Votre enfant n'est pas dans une catégorie définie");
+}
