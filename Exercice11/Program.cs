5397935843ff19f8e3593838ae0ab23f7aeeb1cf
IlyasBajtit <ilyas.bajtit@gmail.com>
2023-12-20 16:28:52 +0100

    feat: add first part ex 11

---
+++ Exercice11/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+Console.WriteLine("--- Le nombre est-il divisible par ...? ---");
+
+Console.Write("Entrez un chiffre/nombre entier : ");
+int nbentier = int.Parse(Console.ReadLine());
+
+Console.Write("Entrez un chiffre/nombre diviseur : ");
+int nbdiviseur = int.Parse(Console.ReadLine());
+
+
+if (nbentier % nbdiviseur == 0)
+{
+     Console.Write($"Le chiffre/nombre est divisible par {nbdiviseur} ");
+}
+else
+{
+    Console.WriteLine($"Le chiffre/nombre n'est pas divisible par {nbdiviseur}");
+}
