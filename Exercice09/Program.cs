5f497ad913e8850f5f3d71c1a59521214745221e
IlyasBajtit <ilyas.bajtit@gmail.com>
2023-12-20 14:34:10 +0100

    feat: add ex 09

---
+++ Exercice09/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Entrez le capital de départ (en euros) :");
+float capitalDedepart = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Entrez le taux d'intérets (en %) :");
+float txInt = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Entrez la durée de l'epargne (en années) :");
+float duree = float.Parse(Console.ReadLine());
+
+double montantInterets = 
