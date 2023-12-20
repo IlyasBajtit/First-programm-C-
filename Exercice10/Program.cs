9fd98360a30dd7106b2222b9fee1d463bfa09c4d
IlyasBajtit <ilyas.bajtit@gmail.com>
2023-12-20 15:58:38 +0100

    feat: add ex 10

---
+++ Exercice10/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("--- La lettre est-elle une voyelle ?");
+
+Console.Write("Entrez une lettre : ");
+string lettre = Console.ReadLine().ToUpper();
+if (lettre == "A" || lettre == "E" || lettre == "I" || lettre == "O" || lettre == "U" || lettre == "Y")
+{
+    Console.Write("Cette lettre est une voyelle !");
+}
+else
+{
+    Console.Write("Cette lettre est une consonne !");
+}
