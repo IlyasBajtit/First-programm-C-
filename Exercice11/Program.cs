e7fa884f254b76c31798b25537266e741b5b8239
IlyasBajtit <ilyas.bajtit@gmail.com>
2023-12-21 12:23:30 +0100

    fix: add correction ex 11

--- Exercice11/Program.cs
+++ Exercice11/Program.cs
@@ -1,4 +1,27 @@
-﻿Console.WriteLine("*** Le nombre est-il divisible par...? ***\n");
+﻿
+
+Console.WriteLine("--- Le nombre est-il divisible par ...? ---");
+
+Console.Write("Entrez un chiffre/nombre entier : ");
+int nbentier = int.Parse(Console.ReadLine());
+
+Console.Write("Entrez un chiffre/nombre diviseur : ");
+int nbdiviseur = int.Parse(Console.ReadLine());
+
+
+if (nbentier % nbdiviseur == 0)
+{
+     Console.Write($"Le chiffre/nombre est divisible par {nbdiviseur} ");
+}
+else
+{
+    Console.WriteLine($"Le chiffre/nombre n'est pas divisible par {nbdiviseur}");
+}
+
+
+Correction:
+
+Console.WriteLine("*** Le nombre est-il divisible par...? ***\n");
 Console.Write("Entrez un chiffre/nombre entier : ");
 //int nbEntier = Convert.ToInt32(Console.ReadLine());
 int nbEntier;
