54ccf91dbddf1add67a5633dd6e78fd2243ea91d
IlyasBajtit <ilyas.bajtit@gmail.com>
2023-12-21 15:57:58 +0100

    fix: ex 22

--- Exercice22/Program.cs
+++ Exercice22/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("--- Les tables de multiplication ---");
+
+
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine("\t Table de " + i);
+
+    for (int j = 1; j <= 10; j++)
+        Console.WriteLine("\t " + i + " x " + j + " = " + (i * j));
+
+
+}
+
