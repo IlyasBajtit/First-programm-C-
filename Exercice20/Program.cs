49c082c97b6dc12cf1cde1e0a8ef4e2db6ba5c01
IlyasBajtit <ilyas.bajtit@gmail.com>
2023-12-21 15:10:55 +0100

    feat: add ex20

--- Exercice20/Program.cs
+++ Exercice20/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("--- Je compte jusqu'a 10");
+
+Console.WriteLine("Je commence a compter : ");
+
+for (int i = 1 ; i <= 10; i++) 
+Console.WriteLine(i);
+
+Console.WriteLine("Super ! Je sais compter jusqu'a 10 ! ");
