153780058c3df314e0c24e3ba1ccfeef28354171
IlyasBajtit <ilyas.bajtit@gmail.com>
2023-12-21 11:35:39 +0100

    feat: add correction ex 07

--- Exercice07/Program.cs
+++ Exercice07/Program.cs
@@ -10,3 +10,25 @@ double hypothenuse = Math.Pow(PremierCote, 2) + Math.Pow(DeuxiemeCote, 2);
 double resultat = Math.Sqrt(hypothenuse);
 
 Console.WriteLine($"La longeur de l'hypothenuse est de  : {resultat}" + "cm");
+
+
+/*Correction:
+
+//Console.WriteLine("");
+//double c1 = double.Parse(Console.ReadLine());
+//double c2 = double.Parse(Console.ReadLine());
+//Console.WriteLine($"hyp est {Math.Sqrt(Math.Pow(c1,2) + Math.Pow(c2,2))}");
+
+
+Console.WriteLine("Saisir la longueur du premier côté (cm) :");
+float cote1 = float.Parse(Console.ReadLine());
+Console.WriteLine("Saisir la longueur du deuxième côté (cm) :");
+float cote2 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Hypothénuse : {Math.Sqrt(Math.Pow(cote1, 2) + Math.Pow(cote2, 2))} cm");
+
+double hyp = Math.Sqrt(Math.Pow(cote1, 2) + Math.Pow(cote2, 2));
+Console.WriteLine($"Hypothénuse : {Math.Round(hyp, 2)} cm");
+Console.WriteLine($"Hypothénuse : {Math.Floor(hyp)} cm");
+Console.WriteLine($"Hypothénuse : {Math.Ceiling(hyp)} cm");
+*/
