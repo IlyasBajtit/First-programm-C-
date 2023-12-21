5792a6601c3eda13a6a63561914164ea52957639
IlyasBajtit <ilyas.bajtit@gmail.com>
2023-12-21 12:21:34 +0100

    feat: add correction ex 10

--- Exercice10/Program.cs
+++ Exercice10/Program.cs
@@ -10,3 +10,24 @@ else
 {
     Console.Write("Cette lettre est une consonne !");
 }
+
+/*Correction:
+
+Console.WriteLine("--- La lettre est-elle une voyelle ? ---".ToUpper());
+Console.Write("Entrez une lettre : ");
+string letter = Console.ReadLine()!.ToUpper();
+//'char premiereLettre = letter[0];'
+
+//'if (letter == "A" '
+//   ' || letter == "E" '
+//   ' || letter == "I" '
+//   ' || letter == "O" '
+//   ' || letter == "U" '
+//   ' || letter == "Y")'
+
+if (letter.Length != 1)
+    Console.WriteLine("Saisie Incorrecte !");
+else if ("AEIOUY".Contains(letter))
+    Console.WriteLine("Cette lettre est une voyelle");
+else
+    Console.WriteLine("Cette lettre n'est pas une voyelle");*/
