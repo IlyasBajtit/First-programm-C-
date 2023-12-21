1e2ff5846f9b0e1fe6a657173302e49738ed6f51
IlyasBajtit <ilyas.bajtit@gmail.com>
2023-12-21 15:22:05 +0100

    feat: add ex21

--- Exercice21/Program.cs
+++ Exercice21/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("--- Menus et sous-menus ---");
+
+Console.WriteLine("Table des matières :");
+
+for (int i = 1; i <= 3; i++)
+{
+    Console.WriteLine("\t\t Chapitre" + i);
+
+    for (int j = 1; j <= 3; j++)
+        Console.WriteLine("\t\t -Partie" + j + "." + j);
+
+}
