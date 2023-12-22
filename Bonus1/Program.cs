8baaa7d0adbb0b55d0642fe3fa1b626ee7a3d54c
IlyasBajtit <ilyas.bajtit@gmail.com>
2023-12-22 11:48:59 +0100

    feat: add bonus 1

--- Bonus1/Program.cs
+++ Bonus1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ //Bonus version tableau
+
+for (int multi = 1; multi <= 10; multi++)
+    Console.Write($"{multi}\t");
+Console.WriteLine();
+Console.WriteLine("----------------------------------------------------------------------------");
+Console.WriteLine(string.Concat(Enumerable.Repeat("-", 60)));
+
+for (int table = 1; table <= 10; table++)
+{
+    for (int multi = 1; multi <= 10; multi++)
+        Console.Write($"{multi*table}\t");
+    Console.WriteLine();
+}
