98a80296a6298a05441601fda78fec43f3fc7b83
IlyasBajtit <ilyas.bajtit@gmail.com>
2023-12-22 11:51:16 +0100

    feat:add bonus2

--- Bonus2/Program.cs
+++ Bonus2/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// En arc en ciel
+
+List<ConsoleColor> colors = new List<ConsoleColor>()
+{
+    ConsoleColor.Red,
+    ConsoleColor.Magenta,
+    ConsoleColor.Blue,
+    ConsoleColor.Cyan,
+    ConsoleColor.Green,
+    ConsoleColor.Yellow
+};
+
+string sousBarre = string.Concat(Enumerable.Repeat("-", largeurCol));
+string barre = "+" + string.Concat(Enumerable.Repeat(sousBarre + "+", largeurTables));
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine(barre);
+
+for (int i = 1; i <= nbTables; i++)
+{
+    Console.ForegroundColor = colors[i % colors.Count];
+    Console.Write("|");
+    for (int j = 1; j <= largeurTables; j++)
+    {
+        Console.ForegroundColor = colors[(i + j) % colors.Count];
+        Console.Write($"{i * j,largeurCol}|");
+    }
+    Console.WriteLine();
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine(barre);
+}
