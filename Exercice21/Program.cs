a1c665e5a127f52ca53a98378ffe0a4d06ecaa7c
IlyasBajtit <ilyas.bajtit@gmail.com>
2023-12-22 11:45:50 +0100

    feat: add correction 21

--- Exercice21/Program.cs
+++ Exercice21/Program.cs
@@ -10,3 +10,14 @@ for (int i = 1; i <= 3; i++)
         Console.WriteLine("\t\t -Partie" + j + "." + j);
 
 }
+
+
+/*Correction:
+
+for (int chapitre = 1; chapitre <= 3; chapitre++)
+{
+    Console.WriteLine("Chapitre " + chapitre);
+    for (int partie = 1; partie <= 3; partie++)
+        Console.WriteLine($"\t-Partie {chapitre}.{partie}");
+}
+*/
