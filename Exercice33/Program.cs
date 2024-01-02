1aae6f6142814b6ea776cfd34fad0609f3c38714
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-01-02 23:04:51 +0100

    feat: add ex33

--- Exercice33/Program.cs
+++ Exercice33/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<char> alphabet = new List<char>();
+
+for (int i = 'A'; i <= 'Z'; i++)
+    alphabet.Add((char)i);
+
+Console.WriteLine("Affichage des valeurs du alphabetleau :");
+
+string espaces = "";
+
+for (int i = 0; i < alphabet.Count; i++)
+{
+    Console.WriteLine(espaces + alphabet[i]);
+    espaces += "  ";
+}
+
+//char[] alphabet = new char[26];
+
+////for (int i = 0; i < 26; i++)
+////    //alphabet[i] = (char)(i + 65);
+////    alphabet[i] = (char)(i + 97);
+
+//for (int i = 'A'; i <= 'Z'; i++)
+//    alphabet[i - 'A'] = (char)i;
+
+//Console.WriteLine("Affichage des valeurs du alphabetleau :");
+
+//string espaces = "";
+
+//for (int i = 0; i < alphabet.Length; i++)
+//{
+//    Console.WriteLine(espaces + alphabet[i]);
+//    espaces += "  ";
+//}
