5e09de6580134f8ec262018e2346c04706356336
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-01-02 16:06:24 +0100

    feat: add ex32

--- Exercice32/Program.cs
+++ Exercice32/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] tab = new int[10];
+string chaine = "";
+
+Console.WriteLine("Insertions des valeurs du tableau: ");
+
+for (int i = 0; i < tab.Length; i++)
+{
+    Console.Write("Saisir la valeur " + (i + 1) + " ");
+    if (int.TryParse(Console.ReadLine(), out int nombre))
+    {
+        tab[i] = nombre;
+    }
+    else
+    {
+        Console.WriteLine("Veuillez entrer une valeur numérique.");
+        i--; // Permet à l'utilisateur de réessayer pour la même position
+    }
+}
+
+Console.WriteLine("Affichage des valeurs du tableau");
+
+for (int i = 0; i < tab.Length; i++)
+{
+    Console.WriteLine(chaine + tab[i]);
+    chaine += "\t";
+}
