a2c9201693ac3b6eb505a6cc3755bc15dfdfe4c2
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-01-03 15:09:40 +0100

    fix: ex40

--- Exercice40/Program.cs
+++ Exercice40/Program.cs
@@ -2,18 +2,12 @@
 
 // Nom complet
 
-/*string Moi(string prenom, string nom)
+string Moi(string prenom, string nom)
 {
     return prenom + " " + nom;
 }
 
 string chaine = Moi("Ilyas", "Bajtit");
-Console.WriteLine(chaine);*/
+Console.WriteLine(chaine);
 
 
-// Soustraction 
-
-double Soustraire(double nombrea, double nombreb)
-{
-    double sommeDesNombres = nombrea / nombreb;
-}
