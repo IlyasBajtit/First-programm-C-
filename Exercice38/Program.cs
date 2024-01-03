4d70f1ab1a3c6472c8799c29b931258ee21de402
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-01-03 11:22:06 +0100

    feat: add ex38

--- Exercice38/Program.cs
+++ Exercice38/Program.cs
@@ -1,1 +1,20 @@
 ﻿Console.WriteLine("*** Ou est passé mon numéros ?");
+int [] tab = new int [10]; 
+Random aleatoire = new Random();
+string chaine = "";
+int nb = 0;
+int index;
+
+Console.WriteLine("Affectation de valeurs... \n");
+
+for (int i = 0; i < tab.Length; i++)
+    tab[i] = aleatoire.Next(1, 51);
+
+Console.WriteLine("Affichage avant tirage : ");
+foreach (var i in tab)
+{
+    Console.WriteLine(chaine + 1);
+    chaine += " ";
+}
+
+nb = tab[0]; 
