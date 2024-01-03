b87ef3218675068426eecb612705334791442f6c
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-01-03 15:14:15 +0100

    feat: add ex40.1

---
+++ Exercice40.1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Soustraction 
+
+double Soustraire(double nombrea, double nombreb)
+{
+    Console.WriteLine($"Je soustrais {nombrea} et {nombreb}");
+    return nombreb - nombreb;
+}
+
+double sub = Soustraire(2, 1);
+Console.WriteLine("Resultat : " + sub);
