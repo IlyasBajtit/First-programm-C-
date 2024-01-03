49946a10579d23c402758504733143354f259736
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-01-03 11:07:20 +0100

    feat: add ex

--- Exercice36/Program.cs
+++ Exercice36/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("*** Tableaux de notes ***");
+
+Console.WriteLine("Combien de notes comporte le tableaux");
+int [] nbNotes = new int[0];
+
