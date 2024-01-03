bad430343bb8c65cab1f85a369b9a898aa0f576f
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-01-03 16:55:08 +0100

    feat: add ex40.3

--- Exercice40.3/Program.cs
+++ Exercice40.3/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Compteur de lettre
+
+int CompteurDeLettreA(string chaine)
+{
+    int compteurA = 0;
+    foreach (char c in chaine.ToLower())
+    {
+        if (c == 'a')
+        {
+            compteurA++;
+        }
+    }
+    return compteurA;
+}
+
+Console.WriteLine(CompteurDeLettreA("C'est le b-a ba"));
+Console.WriteLine(CompteurDeLettreA("mixer"));
