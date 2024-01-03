7c6cc20332ac7ba758ecf152bf063b19d06b1229
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-01-03 11:59:07 +0100

    feat: add ex38

--- Exercice38/Program.cs
+++ Exercice38/Program.cs
@@ -13,8 +13,24 @@ for (int i = 0; i < tab.Length; i++)
 Console.WriteLine("Affichage avant tirage : ");
 foreach (var i in tab)
 {
-    Console.WriteLine(chaine + 1);
+    Console.WriteLine(chaine + i);
     chaine += " ";
 }
 
 nb = tab[0]; 
+
+Array.Sort(tab);
+chaine = " ";
+Console.WriteLine("Aprés : ");
+
+foreach (var i in tab)
+{
+    Console.WriteLine(chaine + i);
+    chaine += " ";
+}        
+
+index =  Array.IndexOf(tab, nb) + 1;
+Console.WriteLine(Environment.NewLine);
+Console.WriteLine($"Le nombre {nb} se trouvait en 1er position");
+Console.WriteLine($"Il se retrouve a la position {index} aprés le tirage");
+Console.Read();
