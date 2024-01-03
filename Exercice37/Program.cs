94354150b9d100864e52fdfaa801b6573531eab5
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-01-03 09:32:39 +0100

    feat: add ex37

--- Exercice37/Program.cs
+++ Exercice37/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] moisAnnee = { "Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre" };
+Console.WriteLine("Enumeration du tableau");
+
+string chaine = "";
+foreach (string mois in moisAnnee)
+{
+    Console.WriteLine(chaine + mois);
+    chaine += "\t";
+
+}
