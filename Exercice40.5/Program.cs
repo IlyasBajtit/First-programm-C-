4e76e914e5bd4be341bcc1d57c87cab5767e9983
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-01-04 10:28:36 +0100

    feat: add ex40.5

---
+++ Exercice40.5/Program.cs
@@ -0,0 +1,12 @@
+﻿(double, double, double,double) Operation(double a, double b)
+{
+    double somme = a + b;
+    double diff = a - b;
+    double quotient = a / b;
+    double produit = a * b;
+    return(somme, diff, quotient, produit);
+}
+
+(double a, double b, double c, double d) = Operation(2, 5);
+
+Console.WriteLine($"La somme est {a}, La difference est {b}, Le quotient est {c}, Le produit est {d} ");
